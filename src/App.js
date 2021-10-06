2b4f0241955927904c49ebf68e3d54df40045275
Syed Fawzul Azim <adnansyed27@gmail.com>
2021-10-06 13:19:50 +0200

    done custom hooks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,50 @@
+import React, { useEffect, useState, useCallback } from 'react';
+
+import Tasks from './components/Tasks/Tasks';
+import NewTask from './components/NewTask/NewTask';
+import useHttp from './components/hooks/use-Http';
+
 function App() {
+
+  const [tasks, setTasks] = useState([]);
+
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp();
+
+
+  useEffect(() => {
+
+    const transformTasks = (taskObj) => {
+      let loadedTask = [];
+
+      for (const taskKey in taskObj) {
+        loadedTask.push({
+          id: taskKey,
+          text: taskObj[taskKey].text,
+        })
+      }
+      setTasks(loadedTask)
+    }
+
+
+    fetchTasks({
+      url: 'https://movies-98f87-default-rtdb.firebaseio.com/tasks.json'
+    }, transformTasks);
+  }, [fetchTasks]);
+
+  const taskAddHandler = (task) => {
+    setTasks((prevTasks) => prevTasks.concat(task));
+  };
+
   return (
-    <div className="App">
-      <h2>React Start</h2>
-    </div>
+    <React.Fragment>
+      <NewTask onAddTask={taskAddHandler} />
+      <Tasks
+        items={tasks}
+        loading={isLoading}
+        error={error}
+        onFetch={fetchTasks}
+      />
+    </React.Fragment>
   );
 }
 
